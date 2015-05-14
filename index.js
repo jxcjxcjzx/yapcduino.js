d1ec20c7c3f6d81efad99cf8330011a6029cb2ba
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-14 11:24:50 +0800

    add setPulse

--- index.js
+++ index.js
@@ -9,4 +9,24 @@ API.analogRead = pcduino.digital.analogRead;
 
 // load extra native API
 var ExtraNative = require('./../build/Release/yapcduino');
-API.setPulse = ExtraNative.setPulse;
+
+/**
+ * Set Pulse
+ *
+ * @param {Int} pin - Pin number
+ * @param {Int} pulse - Pluse in us
+ * @param {Int} period - Period in us, defaults to 20 * 1000
+ * @param {Int} loops - Loop how many times, defaults to 1
+ */
+var setPulse = function(pin, pulse, period, loops) {
+    if (typeof period === "undefined") {
+        period = 20;
+    }
+    if (typeof loops === "undefined") {
+        loops = 1;
+    }
+    ExtraNative.setPulse(pin, pulse, period, loops);
+};
+
+
+module.exports = API;
