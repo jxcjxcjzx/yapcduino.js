8dc8cb5250866bcff7381e93bf7ac2123c827d43
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-22 00:39:13 +0800

    try pthread

--- softpwm/softpwm.cpp
+++ softpwm/softpwm.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+void *fn(void* _highus);
+
+void *fn(void* _highus)
+{
+    for (;;) {
+        int *highus;
+        // int *lowus;
+        highus = (int *) _highus;
+        // lowus = (int *) _lowus;
+        // cout << highus << "," << lowus << endl;
+        cout << *highus << endl;
+    }
+}
 
 int main() {
+    cout << "init softpwm" << endl;
+    pthread_t thread1;
+    int highus = 100;
+    int lowus = 10;
+    pthread_create(&thread1,
+                   NULL,
+                   fn,
+                   (void*) &highus);
+    usleep(100);
+    highus = 200;
+    lowus = 20;
+    usleep(100);
     return 0;
 }
