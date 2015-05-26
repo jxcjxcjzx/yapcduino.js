e63dd5424665d382fabd15a6df7b15ed49e25659
Senorsen <sen@senorsen.com>
2015-05-26 10:59:30 +0800

    Try to fix it under Node.js >= 0.11.0 or io.js

---
+++ src/common.h
@@ -0,0 +1,9 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#if NODE_VERSION_AT_LEAST(0,11,0)
+#define __NODE_V0_11__
+#endif
+
+#endif
+
--- src/yapcduino.cpp
+++ src/yapcduino.cpp
@@ -1,39 +1,64 @@
 #include <node.h>
+#include <node_version.h>
 #include <uv.h>
 #include "./arduino/Arduino.h"
 #include <unistd.h>
 #include <pthread.h>
 #include "./softpwm.h"
+#include "common.h"
 
 using namespace v8;
+using namespace node;
 
 // http://www.arduino.cc/en/Tutorial/Ping
 // http://www.arduino.cc/en/Reference/PulseIn
+#ifdef __NODE_V0_11__
+static void pulseIn(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+#else
 Handle<Value> pulseIn(const Arguments& args) {
     HandleScope scope;
-
+#endif
     int pin = args[0]->NumberValue();
     int value = args[1]->NumberValue(); // type of pulse to read: either HIGH or LOW (int)
     int timeout = args[2]->NumberValue();
 
     int us = pulseIn(pin, value, timeout); // the length of the pulse in us
 
+#ifdef __NODE_V0_11__
+    Local<Number> num = Number::New(isolate, us);
+    args.GetReturnValue().Set(num);
+#else
     Local<Number> num = Number::New(us);
     return scope.Close(num);
+#endif
 }
 
+#ifdef __NODE_V0_11__
+static void usleep(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+#else
 Handle<Value> usleep(const Arguments& args) {
     HandleScope scope;
-
+#endif
     int us = args[0]->NumberValue();
     usleep(us);
 
+#ifndef __NODE_V0_11__
     return scope.Close(Undefined());
+#endif
 }
 
+#ifdef __NODE_V0_11__
+static void setSoftPWM(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+#else
 Handle<Value> setSoftPWM(const Arguments& args) {
-    HandleScope scope;
-
+    HandleScope scope(isolate);
+#endif
     int pin = args[0]->NumberValue();
     int highus = args[1]->NumberValue();
     int lowus = args[2]->NumberValue();
@@ -41,11 +66,17 @@ Handle<Value> setSoftPWM(const Arguments& args) {
 
     set_soft_pwm(pin, highus, lowus, loops_to_live);
 
+#ifndef __NODE_V0_11__
     return scope.Close(Undefined());
+#endif
 }
 
+#ifdef __NODE_V0_11__
+static void setSoftPWM(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
 Handle<Value> setSoftPWMSync(const Arguments& args) {
-    HandleScope scope;
+    HandleScope scope(isolate);
 
     int pin = args[0]->NumberValue();
     int highus = args[1]->NumberValue();
@@ -58,7 +89,7 @@ Handle<Value> setSoftPWMSync(const Arguments& args) {
 }
 
 Handle<Value> getSoftPWMLoopCount(const Arguments& args) {
-    HandleScope scope;
+    HandleScope scope(isolate);
 
     int pin = args[0]->NumberValue();
     int original_loops_to_live = args[1]->NumberValue();
@@ -70,7 +101,7 @@ Handle<Value> getSoftPWMLoopCount(const Arguments& args) {
 }
 
 Handle<Value> unsetSoftPWM(const Arguments& args) {
-    HandleScope scope;
+    HandleScope scope(isolate);
 
     int pin = args[0]->NumberValue();
 
@@ -81,6 +112,7 @@ Handle<Value> unsetSoftPWM(const Arguments& args) {
 
 void Init(Handle<Object> exports)
 {
+
     init(); // important! init is defined in wiring.c
 
     exports->Set(v8::String::NewSymbol("pulseIn"),
